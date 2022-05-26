20c37fe80ed9fc944e0c753ad690959e1758ee3b
lucaxlh13 <lucas.santos@sptech.school>
2022-05-26 15:13:23 -0300

    Script do sql serve esta comentado na pasta 'scripts'.

--- scripts/cacauControl_V3.sql
+++ scripts/cacauControl_V3.sql
@@ -2,9 +2,6 @@ create database CacauControl;
 
 use CacauControl;
 
-
-
-
 create table Cliente(
 idCliente int primary key auto_increment,
 Nome varchar (45),
@@ -43,12 +40,7 @@ FkCliente int,
 foreign key (FkCliente) references Cliente(idCliente)
 );
 
-
-
-
 select* from usuario;
-
-
 
 create table Sensor(
 idSensor int primary key auto_increment,
@@ -81,12 +73,66 @@ FkSensor_dht11 int,
 foreign key (FkSensor_dht11) references Sensor(idSensor)
 );
 
-
-
-
 insert into hist_medicao(dht11_umidade,dht11_temperatura,alerta_temperatura,alerta_umidade,FkSensor_dht11) values
 ('50','40','normal','normal','2');
 
 
 
 
+create table Cliente(
+idCliente int primary key identity,
+Nome varchar (45),
+CEP char (9),
+Numero char (9),
+CNPJ char (19),
+Email varchar (30),
+Celular varchar (20),
+Telefone varchar (20),
+Hectares varchar (15),
+Plano char (10),
+check( Plano='Gold' or Plano='Premium'),
+SituaçãoDoPlano char (15),
+check( SituaçãoDoPlano= 'Ativo' or SituaçãoDoPlano='Inativo'),
+DataDeAquisicao datetime
+);
+
+/*
+
+!!!! script sql serve !!!!
+
+create table usuario(
+idUsuario int primary key identity,
+nome varchar (40),
+cpf char (15),
+email varchar(30),
+senha varchar(45),
+FkCliente int,
+foreign key (FkCliente) references Cliente(idCliente)
+);
+
+create table Sensor(
+idSensor int primary key identity,
+n_serial char(30),
+Latitude varchar(50),
+Longitude varchar(50),
+FkCliente int,
+foreign key (FkCliente) references Cliente(idCliente)
+);
+
+create table hist_medicao(
+idHist int primary key  identity,
+dht11_umidade decimal(10,2),
+dht11_temperatura decimal(10,2),
+luminosidade decimal(10,2),
+lm35_temperatura decimal(10,2),
+chave int,
+momento datetime default current_timestamp,
+Alerta_Temperatura char(15),
+check( Alerta_Temperatura='critico' or Alerta_Temperatura= 'normal' or Alerta_Temperatura='alerta'),
+Alerta_Umidade char(15),
+check(Alerta_Umidade ='critico' or Alerta_Umidade= 'normal' or Alerta_Umidade='alerta'),
+FkSensor_dht11 int,
+foreign key (FkSensor_dht11) references Sensor(idSensor)
+); 
+
+ */
