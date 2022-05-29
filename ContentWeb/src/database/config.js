d88427e63b8ea344fce36aed3be428089a9cb1cb
Mauricio Maxuel <mauricio.msantos@sptech.school>
2022-05-29 12:55:09 -0300

    introdução da lógica de troca de alarmes//incompleto

--- ContentWeb/src/database/config.js
+++ ContentWeb/src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    user: "root",
+    user: "SeuUser",
     database: "CacauControl",
-    password: "balldragon",
+    password: "SuaSenha",
 
 };
 
